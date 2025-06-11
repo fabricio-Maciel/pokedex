6df861de5b3b478a6cef43b2e9302947f772ec11
Fabricio Maciel <fabriciomaciel.dev@gmail.com>
2025-06-11 11:51:33 -0300

    feat: criacao do modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,20 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
-const maxRecords = 15;
-const limit = 5;
+const maxRecords = 151;
 let offset = 0;
+const limit = 5;
+const closeModal = document.getElementById("close-modal");
 
-function convertPokemonToLi(pokemon) {
-  return `<li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-          <ol class="types">
-            ${pokemon.types
-              .map((type) => `<li class="type ${type}">${type}</li>`)
-              .join(" ")}
-          </ol>
-          <img src="${pokemon.photo}" alt="${pokemon.name}">
-        </div>
-      </li>`;
-}
+let allLoadedPokemons = [];
 
 function loadPokemonItens(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    allLoadedPokemons = [...allLoadedPokemons, ...pokemons];
     const newHtml = pokemons
       .map(
-        (pokemon) => `<li class="pokemon ${pokemon.type}">
+        (pokemon, index) => `<li class="pokemon ${pokemon.type}" data-index="${
+          offset + index
+        }"> 
         <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
@@ -38,6 +29,17 @@ function loadPokemonItens(offset, limit) {
       )
       .join(" ");
     pokemonList.innerHTML += newHtml;
+
+    const pokemonItems = document.querySelectorAll(".pokemon");
+    pokemonItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        const index = item.getAttribute("data-index");
+        const pokemonClick = allLoadedPokemons[index];
+        openModal(pokemonClick);
+        console.log("clickou");
+        console.log(pokemonClick);
+      });
+    });
   });
 }
 
@@ -55,4 +57,33 @@ loadMoreButton.addEventListener("click", () => {
   } else {
     loadPokemonItens(offset, limit);
   }
+});
+
+// Modal
+
+function openModal(pokemonTargetClick) {
+  const modal = document.getElementById("modal-id");
+  const modalContent = document.getElementById("modal-content");
+
+  modalContent.innerHTML = `
+      <div class="header-modal">
+        <div class="infos-modal">
+          <span class="name-modal">${pokemonTargetClick.name}</span>
+          <ol class="types-modal">
+            ${pokemonTargetClick.types
+              .map((type) => `<li class="type-modal ${type}">${type}</li>`)
+              .join(" ")}
+          </ol>
+        </div>
+        <span class="number-modal">#${pokemonTargetClick.number}</span>
+      </div>
+      <img src="${pokemonTargetClick.photo}" alt="${pokemonTargetClick.name}">
+  `;
+
+  modal.className = "modal";
+  modal.classList.add(pokemonTargetClick.type);
+}
+
+closeModal.addEventListener("click", () => {
+  document.getElementById("modal-id").classList.add("hidden");
 });
